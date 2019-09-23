4f05e7a72aeae5ff00d39d3d6a1c34e1312e72cd
Kristof <kristof@aut.bme.hu>
2019-09-23 17:27:52 +0200

    Initialized 2019

---
+++ Aruhaz/Aruhaz/InstantiationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aruhaz
+{
+    public class InstantiationTest
+    {
+        [Fact]
+        public void Basics()
+        {
+            var shop = new Shop();
+        }
+    }
+}
