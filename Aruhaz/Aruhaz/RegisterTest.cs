a6f5ed6cee1cee91680c7a5d684812e3779564cf
Viktor Balázs Hőbör <vkhobor@sch.bme.hu>
2019-09-30 11:53:14 +0200

    Unit test : Registered product stays in shop

---
+++ Aruhaz/Aruhaz/RegisterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+
+namespace Aruhaz
+{
+    public class RegisterTest
+    {
+        [Fact]
+        public void RegisteredProductStays()
+        {
+            //arrange
+            var shop = new Shop();
+            shop.Register('A', 10);
+
+            //act
+            var price = shop.Total("A");
+            
+            //assert
+            Assert.Equal(price, 10);
+        }
+
+    }
+}
