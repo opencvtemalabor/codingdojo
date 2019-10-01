d3e59892cc0d7417b7df1eee5066accf89bcaf3a
Marta Boldizsar <marta.boldi13@gmail.com>
2019-10-01 12:11:01 +0200

    RED test: Count discount works

--- Aruhaz/Aruhaz/RegisterTest.cs
+++ Aruhaz/Aruhaz/RegisterTest.cs
@@ -20,5 +20,17 @@ namespace Aruhaz
             Assert.Equal(price, 10);
         }
 
+        [Fact]
+        public void CountDiscountWorks()
+        {
+            var shop = new Shop();
+            shop.Register('A', 10);
+            shop.RegisterCountDiscount('A', 3, 4);
+
+            var price = shop.Total("AAAA");
+
+            Assert.Equal(price, 30);
+        }
+
     }
 }
