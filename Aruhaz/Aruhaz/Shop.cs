f4915d6e94fbf0fb09ba1933938640f0d1109915
Kovacs Akos <akoskovacs1980@gmail.com>
2019-10-01 09:09:39 +0200

    GREEN TEST: Registered product stays in shop

--- Aruhaz/Aruhaz/Shop.cs
+++ Aruhaz/Aruhaz/Shop.cs
@@ -7,11 +7,24 @@ namespace Aruhaz
     class Shop
     {
         protected int price = 0;
+        private Dictionary<char, int> Prices = new Dictionary<char, int>();
         
-        public int Total()
+        public int Total(string param = "")
         {
+            if (param == "")
+            {
+                return 0;
+            }
+            foreach (char item in param)
+            {
+                price += Prices[item];
+            }
             return price;
         }
         
+        public void Register(char product, int productPrice)
+        {
+            Prices[product] = productPrice;
+        }
     }
 }
