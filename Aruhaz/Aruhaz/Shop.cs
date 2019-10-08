0acd77751104867253c4bfc84c4d47d219cd3e5a
pedroo002 <orosz401@gmail.com>
2019-10-08 11:18:10 +0200

    Calculate discount's price properly

--- Aruhaz/Aruhaz/Shop.cs
+++ Aruhaz/Aruhaz/Shop.cs
@@ -8,12 +8,34 @@ namespace Aruhaz
     {
         protected int price = 0;
         private Dictionary<char, int> Prices = new Dictionary<char, int>();
+        private Dictionary<char, string> Discounts = new Dictionary<char, string>();
         
         public int Total(string param = "")
         {
             if (param == "")
             {
                 return 0;
+            }
+            foreach (var discount in Discounts)
+            {
+                if (param.Contains(discount.Key))
+                {
+                    string[] discountDetails = discount.Value.Split(' ');
+                    int paidFor = Convert.ToInt32(discountDetails[0]);
+                    int obtainedAmount = Convert.ToInt32(discountDetails[1]);
+                    int amount = CountProductOccurrence(discount.Key, param);
+                    if (amount >= obtainedAmount)
+                    {
+                        int countedAmount = amount / obtainedAmount;
+                        price += countedAmount * paidFor * Prices[discount.Key];
+                        int i = 0;
+                        while (i < countedAmount * obtainedAmount)
+                        {
+                            param = param.Remove(param.IndexOf(discount.Key), 1);
+                            i++;
+                        }
+                    }
+                }
             }
             foreach (char item in param)
             {
@@ -25,6 +47,23 @@ namespace Aruhaz
         public void Register(char product, int productPrice)
         {
             Prices[product] = productPrice;
+        }
+
+        public void RegisterCountDiscount(char product, int paidFor, int obtainedAmount)
+        {
+            string formatString = paidFor + " " + obtainedAmount;
+            Discounts[product] = formatString;
+        }
+
+        public int CountProductOccurrence(char product, string param)
+        {
+            int count = 0;
+            foreach (char c in param)
+            {
+                if (c == product)
+                    count++;
+            }
+            return count;
         }
     }
 }
