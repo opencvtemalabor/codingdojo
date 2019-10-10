b8a686ae995d70e8c3087d4b9fba2037b2ef3c1b
Kovacs Akos <akoskovacs1980@gmail.com>
2019-10-10 20:38:21 +0200

    MATYAS PELLE's commit. RED test: Amount discount works

--- Aruhaz/Aruhaz/RegisterTest.cs
+++ Aruhaz/Aruhaz/RegisterTest.cs
@@ -32,5 +32,16 @@ namespace Aruhaz
             Assert.Equal(price, 30);
         }
 
+        [Fact]
+        public void AmountDiscountWorks()
+        {
+            var shop = new Shop();
+            shop.Register('A', 10);
+            shop.RegisterAmountDiscount('A', 5, 0.9);
+
+            var price = shop.Total("AAAAA");
+
+            Assert.Equal(price, 45);
+        }
     }
 }
